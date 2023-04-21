bff063856d114b954fd02595b5ad525d3a8a9a0a
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-04-21 21:01:31 +0100

    static props and paths

--- pages/[pid].js
+++ pages/[pid].js
@@ -55,8 +55,8 @@ export async function getStaticProps(context) {
 
 export async function getStaticPaths() {
     const data = await getData();
-    const ids = data.products.map(product => product.id);
 
+    const ids = data.products.map(product => product.id);
     const pathsWithParams = ids.map(id => ({ params: { pid: id } }));
     
     return {
