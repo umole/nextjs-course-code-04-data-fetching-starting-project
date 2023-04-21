5debdc06c6607f03d9d48d3f29f4167865e58e82
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-04-21 20:29:12 +0100

    loding paths dynamically

--- pages/[pid].js
+++ pages/[pid].js
@@ -8,6 +8,10 @@ const ProductDetailPage = (props) => {
 
     const { loadedProduct } = props;
 
+    if (!loadedProduct) {
+        return <p>Loading...</p>
+    }
+
   return (
     <Fragment>
         <h1>{loadedProduct.title}</h1>
@@ -16,42 +20,48 @@ const ProductDetailPage = (props) => {
   )
 }
 
+async function getData() {
+    try {
+        const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+        const JSONData = await fs.readFile(filePath);
+        const data = JSON.parse(JSONData);
+
+        return data;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 export async function getStaticProps(context) {
     const { params } = context;
 
     const productId = params.pid;
+    const data = await getData();
+    const product = data.products.find(product => product.id === productId);
 
-    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-    try {
-        const JSONData = await fs.readFile(filePath);
-        const data = JSON.parse(JSONData);
-
-        const product = data.products.find(product => product.id === productId)
-
-        return {
-            props: {
-                loadedProduct: product
-            }
-        };
-    } catch (error) {
-        console.error(error);
+    if (!product) {
         return {
             notFound: true
         };
     }
-    
 
-   
+    return {
+        props: {
+            loadedProduct: product
+        }
+    }
 }
 
 export async function getStaticPaths() {
+    const data = await getData();
+    const ids = data.products.map(product => product.id);
+
+    const pathsWithParams = ids.map(id => ({ params: { pid: id } }));
+    
     return {
-        paths: [
-            {params: {pid: 'p1'}},
-            {params: {pid: 'p2'}},
-            {params: {pid: 'p3'}},
-        ],
-        fallback: false
+        paths: pathsWithParams,
+        fallback: true
     };
 }
 
