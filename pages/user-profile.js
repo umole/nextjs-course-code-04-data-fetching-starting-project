80f6ccc2470c18c4610880b71c8673de573d2008
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-04-22 16:01:45 +0100

    getServerSidePorps

---
+++ pages/user-profile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const userProfile = (props) => {
+  return (
+    <h1>{props.username}</h1>
+  );
+}
+
+export default userProfile;
+
+export async function getServerSideProps(context) {
+    return {
+        props: {
+            username: Andre
+        }
+    }
+}
